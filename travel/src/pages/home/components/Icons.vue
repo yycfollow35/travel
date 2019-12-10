56b81f0eea0071aac274c961f843d397dcad0870
杨永超 <13234518336@163.com>
2019-12-10 22:36:46 +0800

    update travel/src/pages/home/components/Icons.vue.

--- travel/src/pages/home/components/Icons.vue
+++ travel/src/pages/home/components/Icons.vue
@@ -2,13 +2,13 @@
 <div class="icons">
   <swiper>
     <swiper-slide v-for="(page, index) of pages" :key= "index">
-  <div class="icon" v-for="item of page" :key="item.id">
-    <div class="icon-img">
-    <img class="icon-img-content" :src='item.imgUrl'/>
-    </div>
-    <p class="icon-desc">{{item.desc}}</p>
-  </div>
-  </swiper-slide>
+      <div class="icon" v-for="item of page" :key="item.id">
+        <div class="icon-img">
+          <img class="icon-img-content" :src='item.imgUrl'/>
+        </div>
+        <p class="icon-desc">{{item.desc}}</p>
+      </div>
+    </swiper-slide>
   </swiper>
 </div>
 </template>
