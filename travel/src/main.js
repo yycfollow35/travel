12165825252695130c7e768c1a54abdb5c994ab8
unknown <13234518336@163.com>
2019-12-26 02:41:47 +0800

    finish master

--- travel/src/main.js
+++ travel/src/main.js
@@ -10,6 +10,7 @@ import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
 import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'babel-polyfill'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
